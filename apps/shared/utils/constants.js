151a6c287d07fda67662f1b3a8331e417f6c9dfa
Alex Tiley <alextiley@gmail.com>
2013-11-23 09:49:49 +0000

    using constants for all flash msg references

--- apps/shared/utils/constants.js
+++ apps/shared/utils/constants.js
@@ -2,7 +2,7 @@ module.exports = {
 	PAGE_ID: 'pageId',
 	BODY_CLASSES: 'bodyClasses',
 	FLASH_SUCCESS: 'success',
-	FLASH_ERROR: 'error',
+	FLASH_ERROR: 'errors',
 	FLASH_INFO: 'info',
-	FLASH_WARNING: 'warning'
+	FLASH_WARNING: 'warnings'
 };
