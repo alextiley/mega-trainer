a026ec261d45147fc629dd339b2b5c62aa0e965d
Alex Tiley <alextiley@gmail.com>
2014-03-28 09:04:27 +0000

    restructuring app and fixing errors

---
+++ apps/mega-trainer/controllers/logout.js
@@ -0,0 +1,12 @@
+module.exports.controller = function (app, config, db) {
+
+	var path = require('path'),
+		constants = require(path.join(config.paths.shared.utils, 'constants'));
+
+	app.get('/logout', function (request, response, next) {
+		request.logOut();
+		request.flash(constants.FLASH_SUCCESS, 'You have successfully been logged out.');
+		response.redirect('/login');
+	});
+
+};
